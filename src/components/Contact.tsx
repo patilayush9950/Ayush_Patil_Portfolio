c62ca7eed940d437d001243f88ab7e50fadaf765
patilayush9950 <patilayush9950@gmail.com>
2025-07-26 07:30:16 +0530

    Migrated contact form to Fromspree for email notification

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -141,15 +141,9 @@ const Contact = () => {
                 </CardHeader>
                 <CardContent>
                   {/* --- IMPORTANT CHANGES START HERE --- */}
-                  <form className="space-y-6" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" >
+                  <form className="space-y-6"  method="POST" action="https://formspree.io/f/mzzvrjpb" >
                     {/* Hidden input for Netlify Forms (required) */}
-                    <input type="hidden" name="form-name" value="contact" />
-                    {/* Hidden Honeypot field for spam protection (optional but recommended) */}
-                    <p className="hidden">
-                      <label>
-                        Don’t fill this out if you’re human: <input name="bot-field" />
-                      </label>
-                    </p>
+                    
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                       <div className="space-y-2">
