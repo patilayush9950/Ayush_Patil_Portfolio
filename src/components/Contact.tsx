d0632171e90db149081b643b4fed13dc082269de
patilayush9950 <patilayush9950@gmail.com>
2025-07-26 06:02:53 +0530

    Final step for Netflify+Zapier Integration

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -141,7 +141,7 @@ const Contact = () => {
                 </CardHeader>
                 <CardContent>
                   {/* --- IMPORTANT CHANGES START HERE --- */}
-                  <form className="space-y-6" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" action="/contact-form-submission">
+                  <form className="space-y-6" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
                     {/* Hidden input for Netlify Forms (required) */}
                     <input type="hidden" name="form-name" value="contact" />
                     {/* Hidden Honeypot field for spam protection (optional but recommended) */}
