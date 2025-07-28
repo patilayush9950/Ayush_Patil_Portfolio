36891539fa8e7e81794eba9da73d64756d5fd8c6
patilayush9950 <patilayush9950@gmail.com>
2025-07-28 07:50:02 +0530

    Removed scroll indicator

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -100,12 +100,7 @@ const Hero = () => {
             </Button>
           </div>
 
-          {/* Scroll Indicator */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-float">
-            <div className="w-6 h-10 border-2 border-primary rounded-full flex justify-center">
-              <div className="w-1 h-3 bg-primary rounded-full mt-2 animate-bounce"></div>
-            </div>
-          </div>
+         
         </div>
       </div>
     </section>
